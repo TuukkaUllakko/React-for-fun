6313d598eccafeba6fbe04c0b971a969b1686f73
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-12-20 23:57:51 +0200

    play with useState

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [state, setState] = useState('Change me!')
+  const [newState, setNewState] = useState('')
+
+  const handleNewStateChange = (event) => {
+    event.preventDefault()
+    setNewState(event.target.value)
+  }
+
+  const handleStateChange = (event) => {
+    event.preventDefault()
+    setState(newState)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {state}
+      <form onSubmit={handleStateChange}>
+        <input value={newState} onChange={handleNewStateChange}/>
+        <button type="submit">Change</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
